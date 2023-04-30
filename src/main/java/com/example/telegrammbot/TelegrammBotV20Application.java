93cd9bcfcc6188c0374221da37cfd827ce582e82
93294 <9329496@mail.ru>
2023-04-30 15:18:12 +0300

    Initial commit

---
+++ src/main/java/com/example/telegrammbot/TelegrammBotV20Application.java
@@ -0,0 +1,13 @@
+package com.example.telegrammbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegrammBotV20Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegrammBotV20Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telegrammbot/TelegrammBotV20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegrammbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrammBotV20ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
